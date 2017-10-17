3fee48207c97b0e81b0bad4749335fb5326ac9ba
suweiyang <902860>
2017-10-17 16:54:06 +0800

    global css

---
+++ examples/global-css/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content">
+    <h1 class="title">Another Page</h1>
+    <p>
+      <nuxt-link to="/" class="button is-medium is-info hvr-wobble-vertical">Another button</nuxt-link>
+    </p>
+    <p>
+      <nuxt-link to="/">Back home</nuxt-link>
+    </p>
+  </div>
+</template>
---
+++ examples/global-css/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content">
+    <h1 class="title">Custom CSS!</h1>
+    <p>
+      <nuxt-link to="/about" class="button is-medium is-primary hvr-float-shadow">I am a button</nuxt-link>
+    </p>
+    <p>
+      <nuxt-link to="/about">About page</nuxt-link>
+    </p>
+  </div>
+</template>
