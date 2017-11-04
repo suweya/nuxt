862674a68e3a150afed7e510b70773b26a5f6146
suweya <suweya89@gmail.com>
2017-11-04 12:44:39 +0800

    spa mode

---
+++ examples/spa/nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /**
+   * Single page application mode
+   * Means no SSR
+   */
+  mode: 'spa',
+  /**
+   * Headers of the page (works with SPA!)
+   */
+  head: {
+    title: 'SPA mode with Nuxt.js'
+  }
+}
---
+++ examples/spa/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <p>Hi from {{ name }}</p>
+    <nuxt-link to="/about">About page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      name: process.server ? "server" : "client"
+    };
+  }
+};
+</script>
